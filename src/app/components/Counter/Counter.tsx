5573b3cdd28f6907dbb6a61bf7d8bed44cbab1ed
alexandercannon <alexander.cannon~@farpointlabs.com>
2019-06-11 22:08:57 +0100

    feat(counter): swapped counter class and function

--- src/app/components/Counter/Counter.tsx
+++ src/app/components/Counter/Counter.tsx
@@ -1,4 +1,5 @@
 import Button from "@material-ui/core/Button";
+import { CSSProperties } from "@material-ui/styles";
 import * as React from "react";
 import "./style.css";
 
@@ -6,36 +7,24 @@ interface IProps {
   initialCount?: number;
 }
 
-interface IState {
-  count: number;
-}
+const style: CSSProperties = { "background-color": "blue" };
 
-const increment = Symbol();
-export default class Counter extends React.Component<IProps, IState> {
-  public static defaultProps = {
-    initialCount: 0,
-  };
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      count: props.initialCount!,
-    };
-    this[increment] = this[increment].bind(this);
-  }
-  public [increment] = () => {
-    const { count } = this.state;
-    this.setState({ count: count + 1 });
-  };
-  public render() {
-    const { count } = this.state;
+const Counter = ({ initialCount = 0 }: IProps) => {
+  const [count, setCount] = React.useState(initialCount);
+  React.useEffect(() => {
     document.title = `You clicked ${count} times`;
-    return (
-      <div className="uni-app--counter">
-        <h2>You clicked {count} times</h2>
-        <Button variant="contained" onClick={this[increment]}>
-          Click me
-        </Button>
-      </div>
-    );
-  }
-}
+  });
+
+  const increment = () => setCount(count + 1);
+
+  return (
+    <div className="uni-app--counter">
+      <h2 style={style}>You clicked {count} times</h2>
+      <Button variant="contained" onClick={increment}>
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default Counter;
