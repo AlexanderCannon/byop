18caf5698bf5f6976f069f649cc5b6d7155f4a8c
alexandercannon <alexander.cannon~@farpointlabs.com>
2019-06-12 11:52:53 +0100

    test(*): added global tests

---
+++ src/__tests__/index.browser.spec.ts
@@ -0,0 +1,27 @@
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+import puppeteer from "puppeteer";
+
+expect.extend({ toMatchImageSnapshot });
+let browser: puppeteer.Browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch();
+});
+
+test("renders correctly", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:1234");
+  const image = await page.screenshot();
+  expect(image).toMatchImageSnapshot();
+});
+
+test("renders correctly", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:1234");
+  const image = await page.screenshot();
+  expect(image).toMatchImageSnapshot();
+});
+
+afterAll(async () => {
+  await browser.close();
+});
