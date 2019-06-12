7678af56a443a665ed3f8ddbc9410d98cf1f392e
alexandercannon <alexander.cannon~@farpointlabs.com>
2019-06-12 17:35:01 +0100

    refactor(counter): removed unused import

--- src/app/components/Counter/index.tsx
+++ src/app/components/Counter/index.tsx
@@ -1,6 +1,5 @@
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { CSSProperties } from "@material-ui/styles";
 import * as React from "react";
 import "./style.css";
 
