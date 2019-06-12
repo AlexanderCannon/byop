7c9a628f05c3c48e12f88f3834120b4c1052728e
alexandercannon <alexander.cannon~@farpointlabs.com>
2019-06-12 15:30:59 +0100

    feat(landing): added new image

--- src/app/components/Landing/index.tsx
+++ src/app/components/Landing/index.tsx
@@ -12,7 +12,7 @@ const Landing: React.FunctionComponent = () => (
     </Helmet>
     <img
       className="uni-cmp--splash"
-      src="https://images.unsplash.com/photo-1541185934-01b600ea069c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2551&q=80"
+      src="https://images.unsplash.com/photo-1557128928-66e3009291b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
       height="100%"
       alt="bg-image"
     />
