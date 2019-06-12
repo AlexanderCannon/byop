a50169cba84447057bfd1cabf842c72202597379
alexandercannon <alexander.cannon~@farpointlabs.com>
2019-06-12 11:55:47 +0100

    feat(contitional routing): now has different routes for testing in dev mode

--- src/app/components/Router.tsx
+++ src/app/components/Router.tsx
@@ -1,11 +1,27 @@
 import * as React from "react";
 import { Route, Switch } from "react-router";
+import Counter from "./Counter";
 import Landing from "./Landing";
 import { NotFound } from "./NotFound";
+import Speaker from "./Speaker";
 
 export const Router: React.FunctionComponent = () => (
   <Switch>
+    {process.env.NODE_ENV === "development" ? renderDevComponents() : productionRoutes()}
+  </Switch>
+);
+
+const productionRoutes = () => (
+  <>
     <Route exact={true} path="/" component={Landing} />
     <Route component={NotFound} />
-  </Switch>
+  </>
+);
+
+const renderDevComponents = () => (
+  <>
+    <Route path="/cmp-counter" component={Counter} />
+    <Route path="/cmp-speaker" component={Speaker} />
+    {productionRoutes()}
+  </>
 );
