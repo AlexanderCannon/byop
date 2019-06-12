8385c3caab51e93d21ba4a89bd27df9191b7336f
alexandercannon <alexander.cannon~@farpointlabs.com>
2019-06-12 17:29:45 +0100

    feat(counter): updated awesome counter to use hooks :fire:

--- src/app/components/Counter/index.tsx
+++ src/app/components/Counter/index.tsx
@@ -1,5 +1,6 @@
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { CSSProperties } from "@material-ui/styles";
 import * as React from "react";
 import "./style.css";
 
@@ -7,44 +8,31 @@ interface IProps {
   initialCount?: number;
 }
 
-interface IState {
-  count: number;
-}
-
-const increment = Symbol();
-export default class Counter extends React.Component<IProps, IState> {
-  public static defaultProps = {
-    initialCount: 0
-  };
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      count: props.initialCount!
-    };
-    this[increment] = this[increment].bind(this);
-  }
-  public [increment] = () => {
-    const { count } = this.state;
-    this.setState({ count: count + 1 });
-  };
-  public render() {
-    const { count } = this.state;
+const Counter = ({ initialCount = 0 }: IProps) => {
+  const [count, setCount] = React.useState(initialCount);
+  React.useEffect(() => {
     document.title = `You clicked ${count} times`;
-    return (
-      <div className="byop-app--counter">
-        <Typography variant="h3">
-          You clicked{" "}
-          <span data-testid="byop-app--counter-count-value">{count}</span> times
-        </Typography>
-        <Button
-          className="byop--app-counter-button"
-          data-testid="byop-app--counter-button"
-          variant="contained"
-          color="primary"
-          onClick={this[increment]}>
-          Click me
-        </Button>
-      </div>
-    );
-  }
-}
+  });
+
+  const increment = () => setCount(count + 1);
+
+  return (
+    <div className="byop-app--counter">
+      <Typography variant="h3">
+        You clicked{" "}
+        <span data-testid="byop-app--counter-count-value">{count}</span> times
+      </Typography>
+
+      <Button
+        className="byop--app-counter-button"
+        data-testid="byop-app--counter-button"
+        variant="contained"
+        color="primary"
+        onClick={increment}>
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default Counter;
